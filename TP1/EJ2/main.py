ab5f794d7855360f7d68935d3d6f8671e5c64276
nmestanza <info@nmestanza.com>
2020-08-15 02:58:17 -0300

    Update main.py
    Playing with BODE MatplotLib Functions.
    The script is able to plot BODE diagram for BODE transfer Functions defined.
    It is not capable of plotting multiple plots (just started with two)
    Started to define some classes

--- TP1/EJ2/main.py
+++ TP1/EJ2/main.py
@@ -1,1 +1,106 @@
+import time
+
+from scipy import signal
+import matplotlib.pyplot as plt
+
+
+#####################################################################################################
+# transferNumerator is a list which represents the values of each polynomial coefficient of NUM     #
+# transferDenominator is a list which represents the values of each polynomial coefficient of DEN   #
+# Transfer Function is represented by H(S) = NUM (S)/ DEN (S)                                       #
+# e.g. s^2 + 3s + 5 would be represented as [1, 3, 5]                                               #
+#####################################################################################################
+
+def bodePlotter(transferNumerator, transferDenominator):
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.bode.html
+
+    transferFunction = signal.TransferFunction(transferNumerator, transferDenominator)
+    w, mag, phase = signal.bode(transferFunction)
+
+    transferNumerator2 = [1,1,1]
+    transferDenominator2 = [1, 1]
+    transferFunction2 = signal.TransferFunction(transferNumerator2, transferDenominator2)
+    w2, mag2, phase2 = signal.bode(transferFunction2)
+    plt.ion()
+    plt.figure(1)
+    plt.semilogx(w, mag)  # Bode magnitude plot
+    #plt.semilogx(w2, mag2)
+    plt.xlabel('Decades')
+    plt.ylabel('Gain (Db)')
+    plt.figure(num="PHASE")
+    plt.semilogx(w, phase)  # Bode phase plot
+    plt.xlabel('Angular Frequency')
+    plt.ylabel('Phase (Degree)')
+    #plt.show() This is a blocking function.
+
+    plt.draw()
+
+    plt.figure(1)
+    plt.semilogx(w2, mag2)
+
+
+def main():
+    END = False
+    myBodePlots = bodePlots()
+    transferNumerator = [1]
+    transferDenominator = [1, 1]
+    myFirstBode = bodeFunction(transferNumerator, transferDenominator)
+    while (END == False):
+        plt.pause(0.00001)
+
+
+if __name__ == '__main__':
+    main()
+
+class bodePlots():
+    def __init__(self):
+        self.bodePlotsList = []
+        plt.figure("GAIN")
+        plt.figure("PHASE")
+
+    def addBodePlot (self,bodePlot):
+        self.bodePlotsList.append(bodePlot)
+
+    def removeBodePlot(self,bodePlot):
+        self.bodePlotsList.remove(bodePlot)
+
+    def updatePlot (self):
+        plt.draw()
+
+
+
+
+
+
+#############################################
+# plt.draw () not working
+# After some research
+#https://stackoverflow.com/questions/35215335/matplotlibs-ion-and-draw-not-working
+# The problem - and the solution - is highly dependent on the plot.draw() function within the Python environment and back end, and may even vary in different product releases. It manifests itself in different ways depending on the environment. The problem shows up in many places on stackoverflow with some solutions working for some people and not for others.
+#
+# The gold standard on my Windows laptop is running the Python from the command line - no IDE, just plain vanilla Python3. draw() as shown in the example always works fine there.
+#
+# If I try it in Jupyter notebook on the same machine, no amount of draw(), plot.pause(), plot.show(), or any other suggestion works. I tried %matplotlib with notebook, widget and ipympl. Nothing gets drawn until complete end of cell code execution.
+#
+# Some other sources on stackoverflow suggested using figure.canvas.flush_events(). I had some success with that and investigated further.
+#
+# The best solution turned out to be to run the draw() at the figure.canvas level instead of the axes or plot level.
+
+
+class bodeFunction(transferNumerator, transferDenominator):
+    # https://docs.scipy.org/doc/scipy/reference/generated/scipy.signal.bode.html
+    def __init__(self):
+        self.transferFunction = signal.TransferFunction(transferNumerator, transferDenominator)
+        #signal.bode
+        # Returns
+        # w 1D ndarray
+        # Frequency array [rad/s]
+        #
+        # mag 1D ndarray
+        # Magnitude array [dB]
+        #
+        # phase 1D ndarray
+        # Phase array [deg]
+
+        self.w, self.mag, self.phase = signal.bode(self.transferFunction)
 
