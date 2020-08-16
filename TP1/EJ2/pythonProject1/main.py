38746ed1cd400c5af0967e687f6ef767e83742e2
nmestanza <info@nmestanza.com>
2020-08-16 17:48:28 -0300

    :)
    :)

--- TP1/EJ2/pythonProject1/main.py
+++ TP1/EJ2/pythonProject1/main.py
@@ -1,10 +1,9 @@
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget, QMainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import*
 
-# Matplotlib Modules
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
+
 
 from src.ui.transferFunctionPopUp import Ui_transferFunctionInput
 from src.ui.LTSpicePopUp import Ui_LTSpiceInput
@@ -15,81 +14,80 @@ import src.package.bodeFunctions as bode
 
 from src.ui.bodePlotter import Ui_bodePlotterWindow
 
-# Python Modules
-from numpy import *
-from random import *
-
-
 class myPlot(QMainWindow, Ui_bodePlotterWindow):
     def __init__(self):
         super(myPlot, self).__init__()
         self.setupUi(self)
-        self.figureGain = Figure()
-        self.canvasGain = FigureCanvas(self.figureGain)
-        self.axesGain = self.figureGain.add_subplot()
-        canvasIndexGain = self.plotTableGain.addWidget(self.canvasGain)
-        self.plotTableGain.setCurrentIndex(canvasIndexGain)
-        self.figurePhase = Figure()
-        self.canvasPhase = FigureCanvas(self.figurePhase)
-        self.axesPhase = self.figurePhase.add_subplot()
-        canvasIndexPhase = self.plotTablePhase.addWidget(self.canvasPhase)
-        self.plotTablePhase.setCurrentIndex(canvasIndexPhase)
-
-        self.transferFunctionAction.clicked.connect(self.openTransferFunctionWindow)
+        self.transferFunctionAction.clicked.connect(self.on_plot_update)
+        self.phaseUpdateLabel.clicked.connect(self.updatePhaseLabel)
+        self.gainUpdateLabel.clicked.connect(self.updateGainLabel)
         self.spiceAction.clicked.connect(self.openLTSpiceWindow)
         self.csvAction.clicked.connect(self.openCSVWindow)
 
-    @pyqtSlot()
-    def on_plot_update(self):
-
-        myBode = bode.bodeFunction([1], [1, 1])
-        self.axesGain.clear()
-        self.axesPhase.clear()
-        self.axesGain.semilogx(myBode.w, myBode.mag)
-        self.axesPhase.semilogx(myBode.w, myBode.phase)
-        self.canvasGain.draw()
-        self.canvasPhase.draw()
-
     def openTransferFunctionWindow(self):
-        self.window = QtWidgets.QMainWindow()
+        self.window = QtWidgets.QWidget()
         self.ui = mytransferFunctionPopUp()
         self.ui.setupUi(self.window)
         self.window.show()
 
     def openLTSpiceWindow(self):
-        self.window = QtWidgets.QMainWindow()
+        self.window = QtWidgets.QWidget()
         self.ui = myLTSpicePopUp()
         self.ui.setupUi(self.window)
         self.window.show()
 
     def openCSVWindow(self):
-        self.window = QtWidgets.QMainWindow()
+        self.window = QtWidgets.QWidget()
         self.ui = Ui_CSVInput()
         self.ui.setupUi(self.window)
+        self.window.show()
+
+
+    @pyqtSlot()
+    def on_plot_update(self):
+
+        myBode = bode.bodeFunction([1], [1, 1])
+
+        self.plotTableGain.canvas.axes.clear()
+        self.plotTableGain.canvas.axes.semilogx(myBode.w, myBode.mag)
+        self.plotTableGain.canvas.axes.grid(True,which="both")
+        self.plotTableGain.canvas.axes.title.set_text('Diagrama de BODE - MAGNITUD')
+        self.plotTableGain.canvas.draw()
+        self.plotTablePhase.canvas.axes.clear()
+        self.plotTablePhase.canvas.axes.semilogx(myBode.w, myBode.phase)
+        self.plotTablePhase.canvas.axes.grid(True,which="both")
+        self.plotTablePhase.canvas.axes.title.set_text('Diagrama de BODE - FASE')
+        self.plotTablePhase.canvas.draw()
+
+
+    def updateGainLabel(self):
+        self.plotTableGain.canvas.axes.axes.set_xlabel(self.xLabelInput.text())
+        self.plotTableGain.canvas.axes.axes.set_ylabel(self.yLabelInput.text())
+        self.plotTableGain.canvas.draw()
+
+    def updatePhaseLabel(self):
+        self.plotTablePhase.canvas.axes.axes.set_xlabel(self.xLabelInput.text())
+        self.plotTablePhase.canvas.axes.axes.set_ylabel(self.yLabelInput.text())
+        self.plotTablePhase.canvas.draw()
+
 
 
 class mytransferFunctionPopUp(QWidget, Ui_transferFunctionInput):
     def __init__(self):
         super(mytransferFunctionPopUp, self).__init__()
         self.setupUi(self)
-        #self.inputOkAction.clicked.signal(self.dosomething)
+        self.inputOkAction.clicked.connect(lambda: self.closeWindow(self.transferFunctionInput))
 
-    def dosomething(self):
-        #numerator = self.trasnferNumeratorInput.text()
-        #denominator = self.transferDenominatorInput.text()
+    def closeWindow(self, window):
+        # hide the screen on exit btn clicked
+        window.hide()
         print ("hola")
-        #self.setWindowTitle("HOLIS")
-        #self.
-
 
 class myLTSpicePopUp (QWidget, Ui_LTSpiceInput):
     def __init__(self):
         super(myLTSpicePopUp, self).__init__()
         self.setupUi(self)
-        #self.pushButton.clicked.signal(self.jeje)
 
-    def jeje (self):
-        print ("hola")
 
 class myCSVPopUp (QWidget, Ui_CSVInput):
     def __init__(self):
---
+++ TP1/EJ2/pythonProject1/src/ui/plottablephase.py
@@ -0,0 +1,22 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QWidget, QVBoxLayout
+#from matplotlib.backends.backend_template import FigureCanvas
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
+
+
+class plotTablePhase(QWidget):
+
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+
+        self.canvas = FigureCanvas(Figure())
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+        vertical_layout.addWidget(self.toolbar)
+
+        self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.canvas.axes.title.set_text ('Diagrama de BODE - FASE')
+        self.setLayout(vertical_layout)
